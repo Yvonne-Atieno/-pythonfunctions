d7550f38782d09dd607132db0636d4b815ba0225
NjeriMarion <njeriwmarion@gmail.com>
2023-03-31 07:02:39 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+
+
+def subtract(a,b):
+    answer=a-b
+    return answer
+
+def divide(a,b):
+    answer=a/b
+    return answer
+
+def multiply(a,b):
+    answer=a*b
+    return answer
+
+def remainder(a,b):
+    answer=a%b
+    return answer
---
+++ student.py
@@ -0,0 +1,3 @@
+def year_of_birth(name,age):
+    year=2023-age
+    print(f"hello {name},you were born in {year}")
