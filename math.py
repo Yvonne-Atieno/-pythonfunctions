fc360d8508fdf036bca4b7a5d0f0254f1cff7a17
NjeriMarion <njeriwmarion@gmail.com>
2023-03-31 07:38:19 +0300

    initial commit

--- math.py
+++ math.py
@@ -3,14 +3,15 @@ def add(a,b):
     return answer
 
 
-
 def subtract(a,b):
     answer=a-b
     return answer
 
+
 def divide(a,b):
     answer=a/b
     return answer
+
 
 def multiply(a,b):
     answer=a*b
